79755a57c19b5e22cc91cfbd1caaa106c47d2a57
Ron Almog <ron8132@gmail.com>
2021-11-04 13:17:35 -0400

    use effect, dependencies

--- src/pages/useeffect.js
+++ src/pages/useeffect.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import axios from "axios";
 
 const UseEffect = () => {
   const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
   useEffect(() => {
     console.log("hey useeffect");
     axios.get("https://jsonplaceholder.typicode.com/comments").then((res) => {
@@ -14,6 +17,14 @@ const UseEffect = () => {
     <div>
       <h2>Use Effect</h2>
       <p>Hello {data}</p>
+      <p>Count: {count}</p>
+      <Button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </Button>
     </div>
   );
 };
