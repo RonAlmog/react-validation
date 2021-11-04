8c1bd6a3958dce43fd8cf6a31b84745f17f5b42d
Ron Almog <ron8132@gmail.com>
2021-11-04 09:51:47 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import Formx from "./components/Form";
 import { Container } from "react-bootstrap";
+import About from "./pages/about";
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <Formx />
+        <Routes>
+          <Route path="/" element={<Formx />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </Container>
     </div>
   );
---
+++ src/pages/state.js
@@ -0,0 +1,5 @@
+const State = () => {
+  return <div>state page</div>;
+};
+
+export default State;
